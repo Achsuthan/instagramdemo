4d1a26561b9309693293d4207fd91cd4d98e4bbe
Achsuthan <achsuthan@icloud.com>
2020-03-08 11:52:46 +0530

    1. Data model created for round image
    >>> ach

---
+++ InstagramDemo/Common/Model/RoundImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  RoundImageModel.swift
+//  InstagramDemo
+//
+//  Created by Achsuthan Mahendran on 3/7/20.
+//  Copyright © 2020 Achsuthan Mahendran. All rights reserved.
+//
+
+import Foundation
+
+struct RoundImageModel {
+    var viewHeight: Int = 0
+    var imageHeight: Int = 0
+}
