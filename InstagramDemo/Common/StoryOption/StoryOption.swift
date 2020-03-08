8ffca295d0c2442ab46a61496eae8de3e8b17dd5
Achsuthan <achsuthan@icloud.com>
2020-03-08 11:49:34 +0530

    1. Love component, Comment component, share component and save component added
    >>> ach

---
+++ InstagramDemo/Common/StoryOption/StoryOption.swift
@@ -0,0 +1,60 @@
+//
+//  StoryOption.swift
+//  InstagramDemo
+//
+//  Created by Achsuthan Mahendran on 3/8/20.
+//  Copyright © 2020 Achsuthan Mahendran. All rights reserved.
+//
+
+import UIKit
+
+class StoryOption: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp(){
+        let loveImage  = UIImageView()
+        loveImage.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(loveImage)
+        loveImage.backgroundColor = .lightGray
+        loveImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        loveImage.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        loveImage.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        loveImage.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        
+        let commentImage = UIImageView()
+        commentImage.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(commentImage)
+        commentImage.backgroundColor = .lightGray
+        commentImage.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        commentImage.leadingAnchor.constraint(equalTo: loveImage.trailingAnchor, constant: 15).isActive = true
+        commentImage.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        commentImage.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        let shareImage  = UIImageView()
+        shareImage.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(shareImage)
+        shareImage.backgroundColor = .lightGray
+        shareImage.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        shareImage.leadingAnchor.constraint(equalTo: commentImage.trailingAnchor, constant: 15).isActive = true
+        shareImage.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        shareImage.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        
+        let saveStoryImage  = UIImageView()
+        saveStoryImage.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(saveStoryImage)
+        saveStoryImage.backgroundColor = .lightGray
+        saveStoryImage.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        saveStoryImage.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+        saveStoryImage.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        saveStoryImage.widthAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+}
--- InstagramDemo/ViewController.swift
+++ InstagramDemo/ViewController.swift
@@ -16,16 +16,21 @@ class ViewController: UIViewController {
         return nav
     }()
     
-    
-    let testView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .green
+    let storyView : StoriesView = {
+        let view = StoriesView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    let notificationBackgroundView: UIView = {
-        let view = UIView()
+    let homeTimeLineView : HomeTimeLine = {
+        let view = HomeTimeLine()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    //Notification view need to set the height and width
+    let notifcationView: NotificationView = {
+        let view = NotificationView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -35,6 +40,17 @@ class ViewController: UIViewController {
         self.setUp()
     }
     
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape {
+            print("Landscape")
+        } else {
+            print("Portrait")
+           
+        }
+    }
+    
     private func setUp(){
         self.view.backgroundColor = .white
         navigationController?.navigationBar.isHidden = true
@@ -42,91 +58,23 @@ class ViewController: UIViewController {
         navBar.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
         navBar.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
         navBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
-        
-        
-        self.view.addSubview(self.testView)
-        self.testView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100).isActive = true
-        self.testView.topAnchor.constraint(equalTo: self.navBar.bottomAnchor, constant: 100).isActive = true
-        self.testView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        self.testView.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        self.setUpNotification()
-        
+        navBar.heightAnchor.constraint(equalToConstant: 55).isActive = true
+        self.setUpStoryView()
     }
     
-    private func setUpNotification(){
-        
-        self.view.addSubview(self.notificationBackgroundView)
-        self.notificationBackgroundView.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        self.notificationBackgroundView.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        self.notificationBackgroundView.centerXAnchor.constraint(equalTo: self.testView.centerXAnchor).isActive = true
-        self.notificationBackgroundView.bottomAnchor.constraint(equalTo: self.testView.topAnchor, constant: -2).isActive = true
-        
-        let innerView = UIView()
-        innerView.backgroundColor = .red
-        innerView.translatesAutoresizingMaskIntoConstraints = false
-        self.notificationBackgroundView.addSubview(innerView)
-        innerView.leadingAnchor.constraint(equalTo: self.notificationBackgroundView.leadingAnchor).isActive = true
-        innerView.trailingAnchor.constraint(equalTo: self.notificationBackgroundView.trailingAnchor).isActive = true
-        innerView.topAnchor.constraint(equalTo: self.notificationBackgroundView.topAnchor).isActive = true
-        innerView.bottomAnchor.constraint(equalTo: self.notificationBackgroundView.bottomAnchor, constant: -10).isActive = true
-        innerView.layer.cornerRadius = 6
-        
-        let triangleView = UIView()
-        triangleView.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.notificationBackgroundView.addSubview(triangleView)
-        triangleView.widthAnchor.constraint(equalToConstant: 20).isActive = true
-        triangleView.centerXAnchor.constraint(equalTo: self.notificationBackgroundView.centerXAnchor).isActive = true
-        triangleView.topAnchor.constraint(equalTo: innerView.bottomAnchor, constant: 0).isActive = true
-        triangleView.bottomAnchor.constraint(equalTo: self.notificationBackgroundView.bottomAnchor, constant: 0).isActive = true
-        
-        let heightWidth = 20
-        let path = CGMutablePath()
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x:heightWidth/2, y: heightWidth/2))
-        path.addLine(to: CGPoint(x:heightWidth, y:0))
-        path.addLine(to: CGPoint(x:0, y:0))
-        let shape = CAShapeLayer()
-        shape.path = path
-        shape.fillColor = UIColor.red.cgColor
-        triangleView.layer.insertSublayer(shape, at: 0)
-        
-        let loveView = UIView()
-        loveView.backgroundColor = .blue
-        loveView.translatesAutoresizingMaskIntoConstraints = false
+    private func setUpStoryView(){
+        self.view.addSubview(storyView)
+        self.storyView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 5).isActive = true
+        self.storyView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        self.storyView.topAnchor.constraint(equalTo: self.navBar.bottomAnchor).isActive = true
+        self.storyView.heightAnchor.constraint(equalToConstant: 85).isActive = true
         
         
-        let notificationCountView = UIView()
-        notificationCountView.backgroundColor = .black
-        notificationCountView.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        let stackView = UIStackView(arrangedSubviews: [loveView, notificationCountView])
-        stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
-        stackView.alignment = .center
-        stackView.backgroundColor = .black
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        innerView.addSubview(stackView)
-        stackView.leadingAnchor.constraint(equalTo: innerView.leadingAnchor).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: innerView.trailingAnchor).isActive = true
-        stackView.topAnchor.constraint(equalTo: innerView.topAnchor).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: innerView.bottomAnchor).isActive = true
-        
-        
-        let loveImage = UIImageView()
-        loveImage.translatesAutoresizingMaskIntoConstraints = false
-        loveView.addSubview(loveImage)
-        loveImage.backgroundColor = .gray
-        loveImage.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        loveImage.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        loveImage.trailingAnchor.constraint(equalTo: loveView.leadingAnchor, constant: 0).isActive = true
-        loveImage.centerYAnchor.constraint(equalTo: loveView.centerYAnchor).isActive = true
-        
-        
-        
-        
+        self.view.addSubview(homeTimeLineView)
+        self.homeTimeLineView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 5).isActive = true
+        self.homeTimeLineView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        self.homeTimeLineView.topAnchor.constraint(equalTo: self.storyView.bottomAnchor).isActive = true
+        self.homeTimeLineView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
     
     
